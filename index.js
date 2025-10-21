c39be6ae3709f2b933f691f04f5f7e7cc85e08a8
nabilasaf <nabilasafitri861@gmail.com>
2025-10-21 10:12:30 +0700

    menghubungkan ke database mahasiswa

--- index.js
+++ index.js
@@ -20,3 +20,24 @@ const db = mysql.createConnection({
     database: 'biodata',
     port: 3308
 });
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL: ', + err.stack);
+        return;
+    }
+    console.log('Connection Successfully!');
+});
+
+app.get('/api/mahasiswa ', (req, res) => {
+    db.query('SELECT * FROM mahasiswa', (err, results) => {
+        if (err) {
+            console.error('Error executing query: ', +err.stack);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+        res.json(results);
+    });
+});
+
+
